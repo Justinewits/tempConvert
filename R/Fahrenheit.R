5d641c82389a891c63a7598c6f6cf1d4ff96c490
Justinewits <51783237+Justinewits@users.noreply.github.com>
2019-10-18 14:17:31 +0200

    Delete Fahrenheit.R

--- R/Fahrenheit.R
+++
@@ -1,6 +0,0 @@
-fahrenheit_to_kelvin <- function(temp_F) {
-  # Converts Fahrenheit to Kelvin using fahrenheit_to_celsius() and celsius_to_kelvin()
-  temp_C <- fahrenheit_to_celsius(temp_F)
-  temp_K <- celsius_to_kelvin(temp_C)
-  return(temp_K)
-}
